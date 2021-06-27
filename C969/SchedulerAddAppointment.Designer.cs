8ce2daef0dca67ac89ed1c3b021553e4ab4be7a3
Ryan Wood <46369513+ryanwoodtech@users.noreply.github.com>
2021-06-27 08:35:37 -0700

    WIP: Create AddAppointment form buttons

--- C969/SchedulerAddAppointment.Designer.cs
+++ C969/SchedulerAddAppointment.Designer.cs
@@ -32,6 +32,22 @@ namespace C969
             this.AddAppointmentLabel = new System.Windows.Forms.Label();
             this.AddAppointmentCustomerId = new System.Windows.Forms.Label();
             this.AddAppointmentCustomerComboBox = new System.Windows.Forms.ComboBox();
+            this.AddAppointmentTitleLabel = new System.Windows.Forms.Label();
+            this.AddAppointmentTitleInput = new System.Windows.Forms.TextBox();
+            this.AddAppointmentDescriptionLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // AddAppointmentLabel
@@ -48,7 +64,7 @@ namespace C969
             // 
             this.AddAppointmentCustomerId.AutoSize = true;
             this.AddAppointmentCustomerId.Font = new System.Drawing.Font("Adobe Devanagari", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AddAppointmentCustomerId.Location = new System.Drawing.Point(50, 106);
+            this.AddAppointmentCustomerId.Location = new System.Drawing.Point(50, 100);
             this.AddAppointmentCustomerId.Name = "AddAppointmentCustomerId";
             this.AddAppointmentCustomerId.Size = new System.Drawing.Size(104, 33);
             this.AddAppointmentCustomerId.TabIndex = 1;
@@ -57,16 +73,169 @@ namespace C969
             // AddAppointmentCustomerComboBox
             // 
             this.AddAppointmentCustomerComboBox.FormattingEnabled = true;
-            this.AddAppointmentCustomerComboBox.Location = new System.Drawing.Point(210, 113);
+            this.AddAppointmentCustomerComboBox.Location = new System.Drawing.Point(207, 107);
             this.AddAppointmentCustomerComboBox.Name = "AddAppointmentCustomerComboBox";
             this.AddAppointmentCustomerComboBox.Size = new System.Drawing.Size(167, 21);
             this.AddAppointmentCustomerComboBox.TabIndex = 2;
+            // 
+            // AddAppointmentTitleLabel
+            // 
+            this.AddAppointmentTitleLabel.AutoSize = true;
+            this.AddAppointmentTitleLabel.Font = new System.Drawing.Font("Adobe Devanagari", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddAppointmentTitleLabel.Location = new System.Drawing.Point(96, 145);
+            this.AddAppointmentTitleLabel.Name = "AddAppointmentTitleLabel";
+            this.AddAppointmentTitleLabel.Size = new System.Drawing.Size(58, 33);
+            this.AddAppointmentTitleLabel.TabIndex = 3;
+            this.AddAppointmentTitleLabel.Text = "Title";
+            // 
+            // AddAppointmentTitleInput
+            // 
+            this.AddAppointmentTitleInput.Location = new System.Drawing.Point(207, 150);
+            this.AddAppointmentTitleInput.Name = "AddAppointmentTitleInput";
+            this.AddAppointmentTitleInput.Size = new System.Drawing.Size(167, 20);
+            this.AddAppointmentTitleInput.TabIndex = 4;
+            // 
+            // AddAppointmentDescriptionLabel
+            // 
+            this.AddAppointmentDescriptionLabel.AutoSize = true;
+            this.AddAppointmentDescriptionLabel.Font = new System.Drawing.Font("Adobe Devanagari", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddAppointmentDescriptionLabel.Location = new System.Drawing.Point(33, 205);
+            this.AddAppointmentDescriptionLabel.Name = "AddAppointmentDescriptionLabel";
+            this.AddAppointmentDescriptionLabel.Size = new System.Drawing.Size(121, 33);
+            this.AddAppointmentDescriptionLabel.TabIndex = 5;
+            this.AddAppointmentDescriptionLabel.Text = "Description";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 205);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 100);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(207, 329);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Adobe Devanagari", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 322);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 33);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Title";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(207, 367);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(167, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Adobe Devanagari", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(96, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 33);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Title";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(207, 400);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(167, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Adobe Devanagari", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(96, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 33);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Title";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(207, 433);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(167, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Adobe Devanagari", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(96, 426);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 33);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Title";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(207, 466);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(167, 20);
+            this.textBox6.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Adobe Devanagari", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(96, 459);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 33);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Title";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(207, 499);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(167, 20);
+            this.textBox7.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Adobe Devanagari", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(96, 492);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 33);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Title";
             // 
             // SchedulerAddAppointment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(434, 611);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.AddAppointmentDescriptionLabel);
+            this.Controls.Add(this.AddAppointmentTitleInput);
+            this.Controls.Add(this.AddAppointmentTitleLabel);
             this.Controls.Add(this.AddAppointmentCustomerComboBox);
             this.Controls.Add(this.AddAppointmentCustomerId);
             this.Controls.Add(this.AddAppointmentLabel);
@@ -83,5 +252,21 @@ namespace C969
         private System.Windows.Forms.Label AddAppointmentLabel;
         private System.Windows.Forms.Label AddAppointmentCustomerId;
         private System.Windows.Forms.ComboBox AddAppointmentCustomerComboBox;
+        private System.Windows.Forms.Label AddAppointmentTitleLabel;
+        private System.Windows.Forms.TextBox AddAppointmentTitleInput;
+        private System.Windows.Forms.Label AddAppointmentDescriptionLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label6;
     }
 }
