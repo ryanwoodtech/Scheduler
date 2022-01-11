07988b7867af473d4c0d55078a96875d8b6fd51c
Ryan Wood <46369513+ryanwoodtech@users.noreply.github.com>
2022-01-11 15:22:47 -0800

    Fix out of index error

--- C969/AppointmentByCustomerReport.cs
+++ C969/AppointmentByCustomerReport.cs
@@ -19,24 +19,26 @@ namespace C969
             // Iterate through each customer
             for(int i = 0; i < allCustomerAppointmentData.Count; i++)
             {
-                CustomerAppointmentTextBox.AppendText("Appointments with " + DataAccess.GetCustomerName(int.Parse(allCustomerAppointmentData[i][0][0])));
-                CustomerAppointmentTextBox.AppendText(Environment.NewLine);
-
-                // Iterate thorugh each customer's appointments
-                for(int j = 0; j < allCustomerAppointmentData[i].Count; j++)
+                if (allCustomerAppointmentData[i].Count != 0)
                 {
-                    // Add info about appointment to CustomerAppointmentTextBox
-                    CustomerAppointmentTextBox.AppendText("\tTitle: " + allCustomerAppointmentData[i][j][1]);
+                    CustomerAppointmentTextBox.AppendText("Appointments with " + DataAccess.GetCustomerName(int.Parse(allCustomerAppointmentData[i][0][0])));
                     CustomerAppointmentTextBox.AppendText(Environment.NewLine);
 
-                    CustomerAppointmentTextBox.AppendText("\tStart: " + allCustomerAppointmentData[i][j][2]);
-                    CustomerAppointmentTextBox.AppendText(Environment.NewLine);
+                    // Iterate thorugh each customer's appointments
+                    for(int j = 0; j < allCustomerAppointmentData[i].Count; j++)
+                    {
+                        // Add info about appointment to CustomerAppointmentTextBox
+                        CustomerAppointmentTextBox.AppendText("\tTitle: " + allCustomerAppointmentData[i][j][1]);
+                        CustomerAppointmentTextBox.AppendText(Environment.NewLine);
 
-                    CustomerAppointmentTextBox.AppendText("\tEnd: " + allCustomerAppointmentData[i][j][3]);
-                    CustomerAppointmentTextBox.AppendText(Environment.NewLine);
-                    CustomerAppointmentTextBox.AppendText(Environment.NewLine);
+                        CustomerAppointmentTextBox.AppendText("\tStart: " + allCustomerAppointmentData[i][j][2]);
+                        CustomerAppointmentTextBox.AppendText(Environment.NewLine);
+
+                        CustomerAppointmentTextBox.AppendText("\tEnd: " + allCustomerAppointmentData[i][j][3]);
+                        CustomerAppointmentTextBox.AppendText(Environment.NewLine);
+                        CustomerAppointmentTextBox.AppendText(Environment.NewLine);
+                    }
                 }
-
             }
         }
     }
